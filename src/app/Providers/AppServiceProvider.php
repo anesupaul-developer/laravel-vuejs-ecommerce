0bf56b43d51e2072a8d7ef3faaf3f85c8614b79e
Anesu Ngirande <anesupaulngirande@gmail.com>
2023-06-07 14:44:40 +0200

    add user permission and role resources

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationGroup;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Filament::serving(function () {
+            Filament::registerNavigationGroups([
+                NavigationGroup::make()
+                    ->label('Settings')
+                    ->icon('heroicon-s-cog'),
+                NavigationGroup::make()
+                    ->label('Resources')
+                    ->icon('heroicon-s-collection')
+            ]);
+        });
+
+        Filament::registerNavigationGroups([
+            'Resources',
+            'Settings',
+        ]);
     }
 }
